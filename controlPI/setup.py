ef67a1a730dc4977375cf01d6e46c665d23a63b3
Enkhbold Ganbold <enkhbold470@gmail.com>
2025-04-01 02:35:24 -0700

    Update project structure and documentation: rename package from controlPI to zenbot, enhance .gitignore for Python project files, and improve README with installation instructions and CLI usage examples.

--- controlPI/setup.py
+++ controlPI/setup.py
@@ -1,22 +1,38 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="controlPI",
+    name="zenbot",
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "smbus2>=0.4.2",
-    ],
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="Raspberry Pi to Arduino I2C Motor Controller",
-    long_description=open("README.md").read(),
+    author="Inky Ganbold",
+    author_email="enkhbold470@gmail.com",
+    description="I2C Motor Controller for Raspberry Pi/Arduino robots",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/controlPI",
+    url="https://github.com/enkhbold470",  # Updated URL
+    project_urls={
+        "Bug Tracker": "https://github.com/enkhbold470/rpi-arduino-motor-rx/issues",  # Updated Bug Tracker URL
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Hardware",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
     ],
+    packages=find_packages(),
+    include_package_data=True,
     python_requires=">=3.6",
+    install_requires=[
+        "smbus2>=0.4.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "zenbot=zenbot.main:main",
+        ],
+    },
 ) 
